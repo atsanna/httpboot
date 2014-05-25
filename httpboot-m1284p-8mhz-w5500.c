9351cfbe053909d3e0acdcd0baeae4e8cbcbaf50
Sebastian Wangnick <github@semail.wangnick.de>
2014-05-25 16:13:42 +0200

    License conditions.

--- httpboot-m1284p-8mhz-w5500.c
+++ httpboot-m1284p-8mhz-w5500.c
@@ -1,6 +1,8 @@
 // Internet-Bootloader for Atmega1284p and WizNet W5500, developped using Atmel Studio.
 // See http://s.wangnick.de/doku.php?id=iot-basisstation for target hardware.
 // (C) Copyright 2014 Sebastian Wangnick.
+// Usage under "CC Attribution-Noncommercial-Share Alike 3.0 Unported" as described in http://creativecommons.org/licenses/by-nc-sa/3.0/ is granted.
+
 
 #define F_CPU 8000000
 #define BAUD 57600
